125382375a7acb1a0cc79d732950c0b90b7151e8
carlos <4a.santos@gmail.com>
2022-10-07 21:02:06 +0200

    Added Veriff SDK and code for launching it

--- ios/Plugin/VeriffPlugin.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-@objc public class VeriffPlugin: NSObject {
-    @objc public func echo(_ value: String) -> String {
-        print(value)
-        return value
-    }
-}
--- ios/Plugin/VeriffPluginPlugin.swift
+++ ios/Plugin/VeriffPluginPlugin.swift
@@ -1,18 +1,53 @@
 import Foundation
 import Capacitor
+import Veriff
 
-/**
- * Please read the Capacitor iOS Plugin Development Guide
- * here: https://capacitorjs.com/docs/plugins/ios
- */
 @objc(VeriffPluginPlugin)
 public class VeriffPluginPlugin: CAPPlugin {
-    private let implementation = VeriffPlugin()
+    private var call: CAPPluginCall!
+    
+    @objc func start(_ call: CAPPluginCall) {
+        
+        self.call = call
+        let sessionUrl = call.getString("sessionUrl") ?? ""
+        var configuration = VeriffSdk.Configuration()
+        
+        if let veriffConfiguration: Dictionary<String,String> = call.getObject("configuration") as? Dictionary<String,String> {
+            let branding = VeriffSdk.Branding(themeColor: UIColor.init(hexString: veriffConfiguration["themeColor"]!))
+            configuration = VeriffSdk.Configuration(branding: branding)
+        }
+        
+        let veriff = VeriffSdk.shared
+        veriff.delegate = self
+        DispatchQueue.main.async {
+            veriff.startAuthentication(sessionUrl: sessionUrl, configuration: configuration, presentingFrom: (self.bridge?.viewController!)!)
+        }
+    }
+}
 
-    @objc func echo(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
-        call.resolve([
-            "value": implementation.echo(value)
-        ])
+extension VeriffPluginPlugin : VeriffSdkDelegate {
+    public func sessionDidEndWithResult(_ result: VeriffSdk.Result) {
+        var resultJson: Dictionary<String,String> = [:]
+        switch result.status {
+        case .done:
+            // The user successfully submitted the session
+            resultJson["status"] = "DONE"
+            resultJson["message"] = "Session is completed from clients perspective"
+            break
+        case .canceled:
+            // The user canceled the verification process.
+            resultJson["status"] = "CANCELED"
+            resultJson["message"] = "User canceled the verification process"
+            break
+        case .error(let error):
+            resultJson["status"] = "ERROR"
+            resultJson["message"] = VeriffSdk.Error.init(rawValue: error.rawValue).debugDescription
+            
+        @unknown default:
+            print("Unknown result received from Veriff SDK")
+            return self.call.reject("Unknown result received from Veriff SDK")
+        }
+        
+        self.call.resolve(resultJson)
     }
 }
