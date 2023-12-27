3922c0aa3b9588a9910e4991eb7baf3a4280c140
riley-webb <riley@accesslabs.io>
2023-12-28 08:06:49 +1000

    GWOP Styling

--- ios/Plugin/VeriffPluginPlugin.swift
+++ ios/Plugin/VeriffPluginPlugin.swift
@@ -18,7 +18,6 @@ public class VeriffPluginPlugin: CAPPlugin {
             branding.secondary = UIColor.init(hexString: "#6D11E6")
             branding.onSecondary = UIColor.init(hexString: "#FFFFFF")
             branding.onBackgroundSecondary = UIColor.init(hexString: "#9747FF")
-            branding.cameraOverlay = UIColor.init(hexString: "#FFFFFF")
             branding.background = UIColor.init(hexString: "#FFFFFF")
             branding.success = UIColor.init(hexString: "#06AD5D")
             branding.error = UIColor.init(hexString: "#EA0A0A")
